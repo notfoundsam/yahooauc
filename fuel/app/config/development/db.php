c9efaeccf7c494ccb25c34343f1cd4b46bc434d6
notfoundsam <notfoundsam@gmail.com>
2015-09-14 12:11:29 +0900

    edit env

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,10 +6,10 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host='.getenv('OPENSHIFT_MYSQL_DB_HOST').':'.getenv('OPENSHIFT_MYSQL_DB_PORT').';dbname=fuel_dev4',
-			'username'   => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
-			'password'   => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+			'dsn'        => 'mysql:host=localhost;dbname=fuel_dev4',
+			'username'   => 'root',
+			'password'   => '',
 		),
-		'profiling' => getenv('FUEL_ENV') == 'PRODUCTION' ? false : true
+		'profiling' => true
 	),
 );
