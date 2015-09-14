60483765b669e391f02e74d48fa515e03aea6568
notfoundsam <notfoundsam@gmail.com>
2015-09-15 00:43:27 +0900

    update

---
+++ fuel/app/config/development/config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Part of the Fuel framework.
+ *
+ * @package    Fuel
+ * @version    1.7
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2015 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+return array(
+
+	'profiling'  => true,
+);
