ed4b0471dcde04230c5c5876726ab436bcb78ee5
notfoundsam <notfoundsam@gmail.com>
2015-10-31 18:49:22 +0900

    update db connection

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host='.getenv('OPENSHIFT_MYSQL_DB_HOST').':'.getenv('OPENSHIFT_MYSQL_DB_PORT').';dbname=htmlunit',
+			'dsn'        => 'mysql:host='.getenv('OPENSHIFT_MYSQL_DB_HOST').';port='.getenv('OPENSHIFT_MYSQL_DB_PORT').';dbname=htmlunit',
 			'username'   => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
 			'password'   => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
 		)
