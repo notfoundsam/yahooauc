84ab323ee0b2a06a1ec6d4f886ebea4df5ff3918
notfoundsam <notfoundsam@gmail.com>
2016-04-20 00:28:04 +0900

    update universal template

--- fuel/app/views/admin/universal.php
+++ fuel/app/views/admin/universal.php
@@ -1,6 +1,9 @@
 <?php 
 $redirect = Uri::segment(3) ?  Uri::segment(2).'/'.Uri::segment(3) : Uri::segment(2);
 ?>
+
+<div style="margin:30px;"></div>
+
 <?php if ($items): ?>
 
 <?php foreach ($items as $item): ?>	
@@ -12,73 +15,58 @@ $post_index = '';
 $address = '';
 ?>
 
-			<table class="table table-bordered">
-				<thead>
-					<tr>
-						<th>Count</th>
-						<th>Auction ID</th>
-						<th>Title</th>
-						<th>Price</th>
-						<th>Won date</th>
-						<th>Memo</th>
-						<th></th>
-					</tr>
-				</thead>
-				<tbody>	
-					<?php foreach ($item->auctions as $auction): ?>
-					<?php
-					$count += $auction->item_count;
-					$summ += $auction->price;
-					if (isset($auction->vendor->name) && !in_array($auction->vendor->name, $vendors))
-					{
-						$vendors[] = $auction->vendor->name;
-					}
-					if (isset($auction->vendor->post_index) && !$post_index) 
-					{
-						$post_index = $auction->vendor->post_index;
-					}
-					if (isset($auction->vendor->address) && !$address) 
-					{
-						$address = $auction->vendor->address;
-					}
-					?>
-					<tr>
-						<td><?= $auction->item_count; ?></td>
-						<td><?= $auction->auc_id; ?></td>
-						<td><?= $auction->title; ?></td>
-						<td><?= $auction->price; ?></td>
-						<td><?= Date::create_from_string($auction->won_date, 'mysql')->format('display_date'); ?></td>
-						<td><?= $auction->memo; ?></td>
-						<td style="text-align:right;">
-							<?= Html::anchor('admin/auction/edit/'.$auction->id.'/'.$redirect, 'Edit'); ?> |
-							<?= Html::anchor('admin/auction/delete/'.$auction->id.'/'.$redirect.'?'.\Config::get('security.csrf_token_key').'='.\Security::fetch_token(), 'Delete', array('onclick' => "return confirm('Are you sure?')")); ?>
-						</td>
-					</tr>
-					<?php endforeach; ?>
-				</tbody>
-			</table>
+<?php foreach ($item->auctions as $auction): ?>
+<?php
+$count += $auction->item_count;
+$summ += $auction->price;
+if (isset($auction->vendor->name) && !in_array($auction->vendor->name, $vendors))
+{
+	$vendors[] = $auction->vendor->name;
+}
+if (isset($auction->vendor->post_index) && !$post_index) 
+{
+	$post_index = $auction->vendor->post_index;
+}
+if (isset($auction->vendor->address) && !$address) 
+{
+	$address = $auction->vendor->address;
+}
+?>
+<div class="item-wrapper">
+	<div class="count-circle"></div>
+	<div class="count"><?= $auction->item_count; ?></div>
+	<div class="aucid"><?= $auction->auc_id; ?></div>
+	<div class="title"><?= $auction->title; ?></div>
+	<div class="price"><?= $auction->price; ?></div>
+	<div class="date"><?= Date::create_from_string($auction->won_date, 'mysql')->format('display_date'); ?></div>
+	<div class="action">
+		<span class="glyphicon glyphicon-info-sign"></span>
+		<span class="glyphicon glyphicon-edit"></span>
+		<span class="glyphicon glyphicon-remove"></span>
+	</div>
+</div>
 
-			<table class="table part" >
-				<thead>
-					<tr>
-						<th>Count</th>
-						<th>Ship price</th>
-						<th>Box number</th>
-						<th>Tracking</th>
-						<th>Totall</th>
-						<th>Memo</th>
-					</tr>
-				</thead>
-				<tbody>	<tr>
-						<td><?= $count ?></td>
-						<td><?= $item->price; ?></td>
-						<td><?= $item->box_number; ?></td>
-						<td><?= $item->tracking; ?></td>
-						<td><?= $item->price + $summ; ?></td>
-						<td><?= $item->memo; ?></td>
-					</tr>
-				</tbody>
-			</table>
+<!-- <?= Html::anchor('admin/auction/edit/'.$auction->id.'/'.$redirect, 'Edit'); ?> |
+<?= Html::anchor('admin/auction/delete/'.$auction->id.'/'.$redirect.'?'.\Config::get('security.csrf_token_key').'='.\Security::fetch_token(), 'Delete', array('onclick' => "return confirm('Are you sure?')")); ?> -->
+				
+<?php endforeach; ?>
+
+<div class="part-wrapper">
+	<div class="count-circle"></div>
+	<div class="count"><?= $count ?></div>
+	<div class="tracking">Tracking: 3039-3030-9982<?= $item->tracking; ?></div>
+	<div class="box">Box: 12<?= $item->tracking; ?></div>
+	<div class="ship">Ship: <?= $item->price; ?></div>
+	<div class="items-price">Summ: <?= $summ; ?></div>
+	<div class="total-price">Total: <?= $item->price + $summ; ?></div>
+	<div class="action">
+		<span class="glyphicon glyphicon-arrow-up"></span>
+		<span class="glyphicon glyphicon-arrow-down"></span>
+		<span class="glyphicon glyphicon-info-sign"></span>
+		<span class="glyphicon glyphicon-edit"></span>
+		<span class="glyphicon glyphicon-remove"></span>
+	</div>
+</div>
 		
 			<table class="table" >
 				<tr>
