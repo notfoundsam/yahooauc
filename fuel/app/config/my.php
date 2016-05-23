45382a89d7c2751d2fe5a2631ac665f870889f66
notfoundsam <notfoundsam@gmail.com>
2016-05-23 22:51:13 +0900

    fix env

--- fuel/app/config/my.php
+++ fuel/app/config/my.php
@@ -50,10 +50,7 @@ return [
 
 	// Parse html pages from /tmp directory
 	'test_mode' => [
-		'enabled'      => true,
-		'bidding_page' => APPPATH.'/tmp/yahoo/strong_bid.txt',
-		'won_page'     => APPPATH.'/tmp/yahoo/won.txt',
-		'result_page'  => APPPATH.'/tmp/yahoo/success.txt',
+		'enabled'      => false
 	],
 
 	'groups' => [
