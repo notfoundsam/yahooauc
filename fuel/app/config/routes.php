1abab2d1489cf546a0967bf5111f73c8b2959885
notfoundsam <notfoundsam@gmail.com>
2016-08-27 10:34:55 +0900

    fix routing

--- fuel/app/config/routes.php
+++ fuel/app/config/routes.php
@@ -1,8 +1,8 @@
 <?php
 return array(
-	'_root_'  => 'welcome/index',  // The default route
+	'_root_'  => 'welcome/404',  // The default route
 	'_404_'   => 'welcome/404',    // The main 404 route
-	'admin/bidding/(:num)' => 'admin/bidding/index/$1',
+	// 'admin/bidding/(:num)' => 'admin/bidding/index/$1',
 	
-	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
+	// 'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'),
 );
